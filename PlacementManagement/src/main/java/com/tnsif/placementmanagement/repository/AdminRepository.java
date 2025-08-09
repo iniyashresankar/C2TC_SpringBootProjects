750e11371f7a3843f80e1164c868a5a0dde55ff7
Iniyashre <iniyashresankar@gmail.com>
2025-08-09 22:37:50 +0530

    Delete PlacementManagement/src/main/java/com/tnsif/placementmanagement/repository/AdminRepository.java

--- PlacementManagement/src/main/java/com/tnsif/placementmanagement/repository/AdminRepository.java
+++
@@ -1,7 +0,0 @@
-package com.tnsif.placementmanagement.repository;
-
-import com.tnsif.placementmanagement.entity.Admin;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AdminRepository extends JpaRepository<Admin, Long> {
-}
